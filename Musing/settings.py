e8c6a396832c53b0342d8e5d1026a8c803424aad
riteek sonawane <riteek.s247@gmail.com>
2024-10-02 00:55:24 +0400

    Add user regisration page

--- Musing/settings.py
+++ Musing/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_bootstrap4',
+    'crispy_forms',
+    
 ]
 
 MIDDLEWARE = [
@@ -117,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
